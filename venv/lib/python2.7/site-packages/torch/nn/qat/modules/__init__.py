84e230fb61c9a6ed98f139eb8712a1c929b65bfd
fuchs <fuchs_leo@hotmail.com>
2020-03-30 13:23:41 -0400

    Remove venv

--- venv/lib/python2.7/site-packages/torch/nn/qat/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from .linear import Linear
-from .conv import Conv2d
-
-__all__ = [
-    'Linear',
-    'Conv2d',
-]
